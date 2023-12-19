bba7318911a35725a332ed3a29e4b8cadba22ec8
Tamana543 <tamanafarzami33@gmail.com>
2023-12-19 10:05:45 +0430

    added some javascripts

--- src/java.js
+++ src/java.js
@@ -1,10 +1,40 @@
 function changeTemperature(response) {
   let temperatureElement = document.querySelector("#current-tem-degree");
   let temperature = response.data.temperature.current;
-
+  let descriptionElement = document.querySelector("#description");
   let headingTwo = document.querySelector("#city");
-  headingTwo.innerHTML = response.date.city;
+  let HumidityElement = document.querySelector("#Humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  console.log(date);
+
+  timeElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}M/h`;
+  HumidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
+  headingTwo.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let Days = [
+    "Sunday",
+    "Monnday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = Days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 function showUser(city) {
   let apiKey = "002f53aof18d4ab43a5e5047f8f74ctd";
